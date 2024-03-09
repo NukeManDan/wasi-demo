3a442e35c31dae3f5245c36d400eadd70871cabf
Dan Shields <nukemandan@protonmail.com>
2024-03-08 17:45:40 -0700

    working node cli demo!

--- www/cli.js
+++ www/cli.js
@@ -1,4 +1,10 @@
 // See the README for details on *generation* of the required import
 import { base64 } from "./composed.js";
 
-console.log("Answer (to life) = " + base64.encodeB64("wazzup"));
+let utf8Encode = new TextEncoder();
+let bytes = utf8Encode.encode("Wazzzzup from");
+
+let enc = base64.encodeB64(bytes);
+let dec = base64.decodeStr(enc);
+console.log( "Encode:" + enc);
+console.log( "Decode:" + dec);
